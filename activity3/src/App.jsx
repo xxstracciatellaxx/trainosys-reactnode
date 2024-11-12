4150b677675d013f1b81cac3f636170c59e124de
xxstracciatellaxx <jannelriveraming@gmail.com>
2024-11-12 17:02:37 +0800

    feat:add activity3

--- activity3/src/App.jsx
+++ activity3/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createContext, useContext } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Context = createContext();
 
+const sharedData = {
+  theme: 'light',
+  username: 'Juan',
+};
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Context.Provider value={sharedData}>
+      <ChildComponent />
+    </Context.Provider>
+  );
 }
 
-export default App
+function ChildComponent() {
+
+  const { theme, username } = useContext(Context);
+
+  return (
+    <div>
+      <h1>Welcome, {username}!</h1>
+      <p>Current theme: {theme}</p>
+    </div>
+  );
+}
+
+export default App;
