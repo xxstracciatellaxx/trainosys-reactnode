0b2227d3ee0c3fc7c523122c7ca2fa7b1b54d4b6
xxstracciatellaxx <jannelriveraming@gmail.com>
2024-11-13 09:05:50 +0800

    feat: add activity3state

--- activity3state/src/component/ColorPicker.jsx
+++ activity3state/src/component/ColorPicker.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 const ColorPicker = () => {
-
   const [color, setColor] = useState('red');
 
   const handleColorChange = (event) => {
@@ -9,9 +8,13 @@ const ColorPicker = () => {
   };
 
   return (
-    <div style={{ display: 'flex',textAlign: 'center', marginTop: '50px' }}>
-      <h1 style={{padding: '10px', marginTop: '20px'}}>Choose a Color: </h1>
-      <select value={color} onChange={handleColorChange} style={{ height: '40px',marginTop: '45px',padding: '10px', fontSize: '16px' }}>
+    <div className="container">
+      <h1 className="heading">Choose a Color:</h1>
+      <select
+        value={color}
+        onChange={handleColorChange}
+        className="select"
+      >
         <option value="red">Red</option>
         <option value="green">Green</option>
         <option value="blue">Blue</option>
@@ -19,16 +22,8 @@ const ColorPicker = () => {
         <option value="purple">Purple</option>
       </select>
       <div
-        style={{
-          borderRadius: '10px',
-          marginTop: '20px',
-          width: '100px',
-          height: '100px',
-          backgroundColor: color,
-          margin: 'auto',
-          marginLeft: '10px',
-          border: '1px solid black',
-        }}
+        className="color-box"
+        style={{ backgroundColor: color }}
       ></div>
     </div>
   );
